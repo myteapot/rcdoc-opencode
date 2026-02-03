22aa3a611917ac3d1cd7019268e19818a2222ed4
1198722360 <1198722360@qq.com>
2026-02-03 21:40:41 +0800

    config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,20 +1,10 @@
 import { defineUserConfig } from "vuepress";
-import { viteBundler } from "@vuepress/bundler-vite";
-
 import theme from "./theme.js";
 
-const base = "/docs/";
-
 export default defineUserConfig({
-  base,
   lang: "zh-CN",
   title: "Right Code文档",
   theme,
-  bundler: viteBundler({
-    viteOptions: {
-      base,
-    },
-  }),
 
   // 和 PWA 一起启用
   // shouldPrefetch: false,
